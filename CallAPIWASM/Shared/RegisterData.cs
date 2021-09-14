b7d29293eda879595c9386cb9db274aacf5c0104
Yudha Patria <yudha.patria@mitra10.com>
2021-09-14 11:07:36 +0700

    first

---
+++ CallAPIWASM/Shared/RegisterData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallAPIWASM.Shared
+{
+    public class RegisterData
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string password_confirmation { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+    }
+}
